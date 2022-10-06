b1c4ab4762a08e7d31bab291af65b81d183a449c
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-06 17:03:25 +0100

    2nd attempt

--- median.py
+++ median.py
@@ -9,11 +9,14 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-
+numbers.sort()
+print(numbers)
 halfsize = len(numbers)// 2
 if len(numbers)%2 == 0:
     median = (numbers[halfsize] + numbers[halfsize -1])/2
+    print(f'midpoint is: {halfsize}')
 else:
     median = numbers[halfsize]
+    print(f'midpoint is: {halfsize}')
 print(numbers)
 print(median)
