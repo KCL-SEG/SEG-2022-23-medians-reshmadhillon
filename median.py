2dd6df9ce9e7896aa4d028e07cdca398d48a60bc
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-06 15:45:59 +0100

    attempt1

--- median.py
+++ median.py
@@ -1,6 +1,6 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+median = None
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +9,11 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
+halfsize = len(numbers)// 2
+if len(numbers)%2 == 0:
+    median = (numbers[halfsize] + numbers[halfsize -1])/2
+else:
+    median = numbers[halfsize]
 print(numbers)
+print(median)
